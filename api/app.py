03646309730533f5911e9da4865fd0b57900e560
Ankur Desai <avd123@ic.ac.uk>
2023-10-16 16:59:13 +0100

    gitignore file, and index and hello pages for website

---
+++ api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    input_name = request.form.get("name")
+    input_menu = request.form.get("menu")
+    input_year = request.form.get("year")
+    input_guests = request.form.get("guests")
+    return render_template("hello.html", name=input_name, menu=input_menu, year = input_year, guests = input_guests)
