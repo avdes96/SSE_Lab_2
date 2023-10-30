df66f879710b065032a7d55fa2c58b58c8c2de56
James Hartley <james.hartley23@imperial.ac.uk>
2023-10-30 15:41:53 +0000

    completed test

--- api/app_test.py
+++ api/app_test.py
@@ -15,4 +15,4 @@ def test_knows_name():
     assert process_query("What is your name?") == "james_ankur"
 
 def test_largest_number():
-    assert process_query("Which of the following numbers is the largest: {nums[0]}, {nums[1]}, {nums[2]}")
+    assert process_query("Which of the following numbers is the largest: {nums[0]}, {nums[1]}, {nums[2]}") == max(nums)
