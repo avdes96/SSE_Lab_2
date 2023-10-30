d1c9663a31c952c4dfeb58ef1b70e9b00a49bc0e
Ankur Desai <avd123@ic.ac.uk>
2023-10-30 15:52:36 +0000

    Removed test

--- api/app_test.py
+++ api/app_test.py
@@ -14,5 +14,5 @@ def test_does_not_know_about_asteroids():
 def test_knows_name():
     assert process_query("What is your name?") == "james_ankur"
 
-def test_largest_number():
-    assert process_query("Which of the following numbers is the largest: {nums[0]}, {nums[1]}, {nums[2]}") == max(nums)
+#def test_largest_number():
+#    assert process_query("Which of the following numbers is the largest: {nums[0]}, {nums[1]}, {nums[2]}") == max(nums)
