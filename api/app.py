73a1f55bd0c0be62854bcd74e507543f8404cc4d
Ankur Desai <avd123@ic.ac.uk>
2023-10-30 16:39:43 +0000

    Updated test

--- api/app.py
+++ api/app.py
@@ -35,28 +35,31 @@ def process_query(entity):
     elif entity == "What is your name?":
         return "james_ankur"
     elif entity.startswith("Which of the following numbers is the largest"):
-        numbers = [int(num) for num in re.findall(r'\d+', entity)]
+        numbers = [int(num) for num in re.findall(r"\d+", entity)]
         last_three_numbers = numbers[-3:]
         largest = max(last_three_numbers)
         return str(largest)
     elif "What is" in entity and "plus" in entity:
-        numbers = [int(num) for num in re.findall(r'\d+', entity)]
+        numbers = [int(num) for num in re.findall(r"\d+", entity)]
         last_two_numbers = numbers[-2:]
         total = sum(last_two_numbers)
         return str(total)
     elif "What is" in entity and "multiplied" in entity:
-        numbers = [int(num) for num in re.findall(r'\d+', entity)]
+        numbers = [int(num) for num in re.findall(r"\d+", entity)]
         last_two_numbers = numbers[-2:]
         result = last_two_numbers[0] * last_two_numbers[1]
         return str(result)
-    
-    elif startswith("Which of the following numbers is both a square and a cube:"):
-        numbers = [int(num) for num in re.findall(r'\d+', entity)]
+
+    elif entity.startswith(
+        "Which of the following numbers is both a square and a cube:"
+    ):
+        numbers = [int(num) for num in re.findall(r"\d+", entity)]
         for num in numbers:
             if is_cube(num) and is_square(num):
                 return str(num)
 
     return "Please query for dinosaurs or asteroids!"
+
 
 @app.route("/query", methods=["GET"])
 def query():
@@ -64,10 +67,12 @@ def query():
     info = process_query(q)
     return info
 
+
 def is_cube(number):
-    cube_root = round(number **(1/3))
-    return cube_root ** 3 == number
+    cube_root = round(number ** (1 / 3))
+    return cube_root**3 == number
+
 
 def is_square(number):
-    sq_root = round(number **(1/2))
-    return sq_root ** 2 == number
+    sq_root = round(number ** (1 / 2))
+    return sq_root**2 == number
--- api/app_test.py
+++ api/app_test.py
@@ -3,26 +3,29 @@ from app import process_query
 
 def test_knows_about_dinosaurs():
     assert (
-        process_query("dinosaurs")
-        == "Dinosaurs ruled the Earth 200 million years ago"
+        process_query("dinosaurs") == "Dinosaurs ruled the Earth 200 million years ago"
     )
 
 
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
 
+
 def test_knows_name():
     assert process_query("What is your name?") == "james_ankur"
+
 
 def test_sum():
     query = "What is 42 plus 76?"
     expected = str(118)
     assert process_query(query) == expected
 
+
 def test_largest():
     query = "Which of the following numbers is the largest: 53, 1, 35"
     expected = str(53)
     assert process_query(query) == expected
+
 
 def test_sq_cube():
     query = "Which of the following numbers is both a square and a cube: 216, 891, 256, 2201, 1285, 4395, 4096?"
