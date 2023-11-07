7632303713259040823b83353989abd2211c4d68
Ankur Desai <avd123@ic.ac.uk>
2023-11-07 16:38:09 +0000

    Changed string checking to startswith

--- api/static/functions.js
+++ api/static/functions.js
@@ -80,7 +80,7 @@ function get_compliment() {
         })
         .then(response => response.text())
         .then(data => {
-            if (data === "An error occurred with your deployment") {
+            if (data.startsWith() === "An error occurred with your deployment") {
                 data = "ChatGPT took long to respond, please try again!"
             }
             console.log(data)
